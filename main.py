ba50f20695344e3872b8abaaa6a8f3afc5761e48
agungibnu <agung.ibnughani@gmail.com>
2021-03-20 23:33:00 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+import serial
+import datetime
+import time
+import csv
+
+comport = 'COM7'
+baudrates = 9600
+openserial =  serial.Serial(comport, baudrates)
+ts = time.time()
+
+data = openserial.readline().decode('ascii')
+datasplit = data.split(',')
+
+waktu = datetime.datetime.fromtimestamp(ts).strftime("%H%M")
+humidity = datasplit [0]
+temperature = datasplit [1]
+
+hum = str(humidity)
+temp = str(temperature)
+
+print(humidity)
+print(temperature) 
+
+
+logname = datetime.datetime.fromtimestamp(ts).strftime("%Y%m%d") + ".csv"
+while True :
+  
+  
+    
+  
+
+
+
+
+
+
+
